903b796807eae94d5dd00d2c9d6b836255b18d46
navjotmaan <navjotmaan137@gmail.com>
2025-12-20 15:47:30 +0000

    add navigation bar and outlet to display children

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import '../App.css'
 
 const Home = () => {
     return (
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='shop'>Shop</Link></li>
-                <li><Link to='cart'>Cart</Link></li>
-            </ul>
-        </nav>
+        <div>
+            <nav className="navigation-bar">
+                <Link to='/' className="link">Home</Link>
+                <Link to='shop' className="link">Shop</Link>
+                <Link to='cart' className="link">Cart</Link>
+            </nav>
+
+            <Outlet />
+        </div>
     )
 };
 
