b0c432954f5001275c39bcdceae22fe4a9c98268
navjotmaan <navjotmaan137@gmail.com>
2025-12-21 06:26:28 +0000

    fix the routes by making App root and other its children

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+import App from './components/App';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
@@ -6,9 +7,13 @@ import Error from './components/Error';
 const routes = [
     {
         path: '/',
-        element: <Home />,
+        element: <App />,
         errorElement: <Error />,
         children: [
+            {
+                index: true,
+                element: <Home />,
+            },
             {
                 path: 'shop',
                 element: <Shop />,
