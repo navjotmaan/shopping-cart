cf516ae4d358a30b5402bbafe3be6fc26c017e50
navjotmaan <navjotmaan137@gmail.com>
2025-12-21 07:51:58 +0000

    add image to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
-import '../App.css'
+import shopping from '../assets/shopping.png';
 
 const Home = () => {
     return (
         <div>
-            <nav className="navigation-bar">
-                <Link to='/' className="link">Home</Link>
-                <Link to='shop' className="link">Shop</Link>
-                <Link to='cart' className="link">Cart</Link>
-            </nav>
+            
+             <div className="shopping-girl">
+                <img src={shopping} alt="" />
+            </div>
 
-            <Outlet />
         </div>
     )
 };
