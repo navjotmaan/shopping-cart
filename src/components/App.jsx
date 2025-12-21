fe02fca65b3220046f76792e021dc63b10413b12
navjotmaan <navjotmaan137@gmail.com>
2025-12-21 06:23:39 +0000

    make App.jsx root file containing navigation bar

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
+import '../App.css'
+
+const App = () => {
+    return (
+        <div>
+            <nav className="navigation-bar">
+                <h1>ZOOHO</h1>
+                <div className="links-bar">
+                    <Link to='/'>Home</Link>
+                    <Link to='shop'>Shop</Link>
+                    <Link to='cart'>Cart</Link>
+                </div>
+            </nav>
+
+            <Outlet />
+        </div>
+    )
+};
+
+export default App;
