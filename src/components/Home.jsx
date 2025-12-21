0a6f6e8e9b39e4fe758e7e34d1360941145abc1d
navjotmaan <navjotmaan137@gmail.com>
2025-12-21 08:53:03 +0000

    add home page content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
 import shopping from '../assets/shopping.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div>
-            
-             <div className="shopping-girl">
-                <img src={shopping} alt="" />
+        <div className="shopping-girl">
+            <div className='home-content'>
+                <h2>Find Something You’ll Love</h2>
+                <p>From unique finds to daily necessities, we’ve got everything you need to make your house a home.</p>
+                <button><Link to="shop">Start Shopping</Link></button>
             </div>
-
+            <img src={shopping} alt="" />
         </div>
     )
 };
