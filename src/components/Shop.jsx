f61c24a0e3ba274acbd3c8f2973cec65e2e4d251
navjotmaan <navjotmaan137@gmail.com>
2025-12-21 15:11:41 +0000

    change Fetch.jsx name to Shop.jsx

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,55 @@
-import Fetch from "../Fetching/Fetch";
+import { useState, useEffect } from "react";
+import Items from "./Items";
 
-const Shop = () => {
+function Shop() {
+    const [cards, setCards] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        fetch("https://fakestoreapi.com/products", {signal})
+        .then((res) => res.json())
+        .then((data) => {
+            const products = data.map((product) => ({
+                id: product.id,
+                img: product.image,
+                title: product.title,
+                description: product.description,
+                price: product.price,
+            }));
+            setCards(products);
+            setLoading(false);
+        })
+        .catch((err) => {
+            if (err.name === 'AbortError') {
+                return;
+            } else {
+                console.error('Fetch error:', err);
+                setLoading(false);
+            }
+        });
+
+        return () => controller.abort();
+    }, []);
+
+    if (loading) return <p className="loading">Loading ...</p>
+
     return (
-
-        <div>
-            <Fetch />
+        <div className="shop">
+            {cards.map((card) => (
+                <div key={card.id} className="product">
+                    <img src={card.img} alt={card.title} className="picture"/>
+                    <p><strong>{card.title}</strong></p>
+                    <p className="description">{card.description}</p>
+                    <p><strong>${card.price}</strong></p>
+                    <Items />
+                    <button>Add to cart</button>
+                </div>
+            ))}
         </div>
     )
-};
+}
 
 export default Shop;
