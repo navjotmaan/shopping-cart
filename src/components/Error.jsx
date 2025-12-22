5624add5eb9daeaee6cc36d86bc552b6ae6a6a1d
navjotmaan <navjotmaan137@gmail.com>
2025-12-22 03:31:45 +0000

    add class name to error div to style the error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
+        <div className="error-page">
             <h1>Oh! this route does not exist</h1>
             <Link to='/'>
                 You can go back to home page by clicking here!
