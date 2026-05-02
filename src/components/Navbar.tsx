bc0002bac3619a94b2fb8aabed37ff50e720f7cc
navjotmaan <navjotmaan137@gmail.com>
2026-05-02 09:43:47 +0000

    create different file for navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import type { CartItem } from "./Interface.js";
+
+const Navbar = ({ cartItems }: { cartItems: CartItem[] }) => {
+    return (
+        <nav className="flex justify-between fixed top-0 items-center z-50 py-5 md:px-10 px-8 w-full">
+                <h1 className="bg-[#ecf39e] text-[#132a13] py-2 px-10 rounded-3xl md:text-3xl tracking-[0.2em] font-bold font-home">EPIKCART</h1>
+                <div className="flex items-center gap-5 md:gap-10">
+                    <Link to='/'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
+                    </Link>
+
+                    <Link to='shop'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-store-icon lucide-store"><path d="M15 21v-5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v5"/><path d="M17.774 10.31a1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.451 0 1.12 1.12 0 0 0-1.548 0 2.5 2.5 0 0 1-3.452 0 1.12 1.12 0 0 0-1.549 0 2.5 2.5 0 0 1-3.77-3.248l2.889-4.184A2 2 0 0 1 7 2h10a2 2 0 0 1 1.653.873l2.895 4.192a2.5 2.5 0 0 1-3.774 3.244"/><path d="M4 10.95V19a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8.05"/></svg>
+                    </Link>
+
+                    <Link to='cart' className="relative inline-flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg> 
+                        <span className="absolute -top-3 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-[10px] font-bold text-white">
+                            {cartItems.length}
+                        </span>
+                    </Link>
+                </div>
+            </nav>
+    );
+}   
+
+export default Navbar;
